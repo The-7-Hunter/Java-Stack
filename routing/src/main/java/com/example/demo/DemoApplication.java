ca39515103ee9016eae4c7f727d1c24fa68b1709
foo <nawafhunter@gmail.com>
2019-11-06 12:58:50 +0300

    Upload routing Task

---
+++ routing/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}	
+	
+	@RequestMapping("/")
+	public String index(@RequestParam(value = "q", required = false) String searchQuery) {
+		return "You searched for: " + searchQuery;
+	}
+	
+    @RequestMapping("/m/{track}/{module}/{lesson}")
+    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
+    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
+    }
+}
+		
