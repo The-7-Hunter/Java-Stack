4a23381b3c1381f6423b27964f750e78fbdb6ad3
foo <nawafhunter@gmail.com>
2019-11-08 21:47:26 +0300

    date and time task

---
+++ date_time/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+//@RestController
+@Controller
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+//	
+//	@RequestMapping("/")
+//	public String index(@RequestParam(value = "first", required = false, defaultValue = "Human") String searchQuery, @RequestParam(value = "last", required = false, defaultValue = "Human") String searchQuery1) {
+//		return "Name: " + searchQuery + searchQuery1;
+//	}
+//	
+//    @RequestMapping("/m/{track}/{module}/{lesson}")
+//    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
+//    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
+//    }
+
+	@RequestMapping("/testing_render")
+	public String HomePage(Model model) {
+		model.addAttribute("Name", "Abdullelah");
+		return "index.jsp";
+	}
+
+//	@RequestMapping("/")
+//	public String Home(Model model) {
+//		model.addAttribute("Name", "Nawaf");
+//		return "index.html";
+//	}
+	@RequestMapping("/date")
+	public String date(Model model) {
+		model.addAttribute("date", new java.util.Date());
+		return "date.jsp";
+	}
+
+	@RequestMapping("/time")
+	public String time(Model model) {
+		model.addAttribute("time", new java.util.Date());
+		return "time.jsp";
+	}
+}
