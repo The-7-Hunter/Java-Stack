60d097561ac26990c85bce8fe760f5c58e9a88eb
The-7-Hunter <nawafhunter@gmail.com>
2019-11-11 14:52:55 +0300

    MVC Full CRUD using APIs only

---
+++ MVC/src/main/java/com/example/demo/services/BookService.java
@@ -0,0 +1,82 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.Book;
+import com.example.demo.repositories.BookRepository;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    
+
+    public BookService(BookRepository bookRepository) {
+
+        this.bookRepository = bookRepository;
+
+    }
+
+    // returns all the books
+
+    public List<Book> allBooks() {
+
+        return bookRepository.findAll();
+
+    }
+
+    // creates a book
+
+    public Book createBook(Book b) {
+
+        return bookRepository.save(b);
+
+    }
+
+    // retrieves a book
+
+    public Book findBook(Long id) {
+
+        Optional<Book> optionalBook = bookRepository.findById(id);
+
+        if(optionalBook.isPresent()) {
+
+            return optionalBook.get();
+
+        } else {
+
+            return null;
+
+        }
+
+    }
+    public Book ubdateBook(Book book) {
+    	return bookRepository.save(book);
+    }
+   
+    // Edit a book 
+   public Book updateBook(long id, String title, String description, String language, Integer numberOfPages) {
+	Optional <Book> bookToEdit = bookRepository.findById(id);
+	if(bookToEdit != null) {
+		bookToEdit.get().setTitle(title);
+		bookToEdit.get().setDescription(description);
+		bookToEdit.get().setLanguage(language);
+		bookToEdit.get().setNumberOfPages(numberOfPages);
+		
+		return bookToEdit.get();
+	}
+	return null;
+}
+   
+  public void deleteBook(long id) {
+	  bookRepository.deleteById(id);
+  }
+    
+}
+
+
+
+
